22e2709cbb71aebe219bfd3d9f4c1ad722e62663
Moon He Jow <gmlwh124@naver.com>
2022-08-28 22:00:18 +0900

    Test commit

--- src/main/java/artistep/version1/v1domain/majorPost/post/dto/PostRequestDto.java
+++ src/main/java/artistep/version1/v1domain/majorPost/post/dto/PostRequestDto.java
@@ -1,4 +1,42 @@
 package artistep.version1.v1domain.majorPost.post.dto;
 
+import artistep.version1.v1domain.majorPost.post.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
 public class PostRequestDto {
+    @Data
+    @NotNull
+    public static class UpdateTitleForm {
+        private String title;
+    }
+
+    @Data
+    @NotNull
+    public static class UpdateContentForm {
+        private String content;
+    }
+
+    @Data
+    @NotNull
+    public static class UpdateCategoryForm {
+        private String category;
+    }
+
+    @Data
+    @NotNull
+    public static class UpdateUpdatedForm {
+        private LocalDateTime Updated;
+    }
+
+    @Data
+    @NotNull
+    public static class UpdateStatusForm {
+        private String status;
+    }
 }
